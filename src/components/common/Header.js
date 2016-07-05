3fedee195719f1c59a2c7441abb74826d81268cf
Eddo W. Hintoso <eddo.hintoso+github@gmail.com>
2016-07-05 16:48:37 +0800

    Presentational component for ongoing AJAX calls

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -7,6 +7,8 @@
 import React, {PropTypes} from 'react';
 import {Link, IndexLink} from 'react-router';
 
+import LoadingDots from 'components/common/LoadingDots';
+
 
 const Header = () => {
   return (
@@ -16,6 +18,7 @@ const Header = () => {
       <Link to="/courses">Courses</Link>
       {" | "}
       <Link to="/about">About</Link>
+      <LoadingDots interval={100} dots={20} />
     </nav>
   )
 }
